f87a2cf11b545765e21f7760637a867ed8d6c881
Athmik A <athmik27@gmail.com>
2026-08-30 20:22:41 +0530

    Uncomment age filtering logic in filtering_numpy.py
    Uncommented code to filter and print adult ages based on conditions.

--- filtering_numpy.py
+++ filtering_numpy.py
@@ -5,13 +5,13 @@ import numpy as np
 ages=np.array([[20,25,30],
                [35,40,45]])
 
-# adults=ages[ages>30]
-# print(adults)
+adults=ages[ages>30]
+print(adults)
 
-# adults=ages[(ages>=25) & (ages<=40)]
-# adults=ages[(ages>=25) | (ages<=40)]
+adults=ages[(ages>=25) & (ages<=40)]
+adults=ages[(ages>=25) | (ages<=40)]
 
-# print(adults)
+print(adults)
 
 # when we print the array if we need to preserve an original shape we use 'where()' func
 adults=np.where(ages>20,ages,0) # syntax is 
